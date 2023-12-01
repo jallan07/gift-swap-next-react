0d6a4a1e3acaf7a97534fee51ef7b71809460134
Josh Allan <allan.josh07@gmail.com>
2023-12-01 16:15:54 -0500

    Update copy to clipboard text

--- src/app/helpers/pairingsToString.ts
+++ src/app/helpers/pairingsToString.ts
@@ -1,4 +1,4 @@
 export const pairingsToString = (pairs: { sender: string; receiver: string; }[]): string => {
-    const res = pairs.map(p => `${p.sender} -> ${p.receiver}`).join('\n');
+    const res = pairs.map(p => `${p.sender.toUpperCase()} is assigned to give a gift to ${p.receiver.toUpperCase()}`).join('\n');
     return res;
 }
