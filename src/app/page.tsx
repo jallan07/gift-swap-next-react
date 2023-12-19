d4ee578278b46acc0ffc428b861a6c053d07720e
Josh Allan <allan.josh07@gmail.com>
2023-12-19 15:28:10 -0500

    update link to personal site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,29 @@
-'use client'
-import { giftSwap } from './helpers/giftSwap'
-import { useEffect, useState } from 'react'
-import { Box, Divider, TextField } from '@mui/material'
-import TextButton from './common-components/TextButton'
-import Pairings from './common-components/Pairings'
-import { pairingsToString } from './helpers/pairingsToString'
+"use client";
+import { giftSwap } from "./helpers/giftSwap";
+import { useEffect, useState } from "react";
+import { Box, Divider, TextField } from "@mui/material";
+import TextButton from "./common-components/TextButton";
+import Pairings from "./common-components/Pairings";
+import { pairingsToString } from "./helpers/pairingsToString";
 
 export default function Home() {
-  const [inputState, setInputState] = useState<string>('')
-  const [giftSwapState, setGiftSwapState] = useState<{ sender: string; receiver: string; }[] | []>([])
+  const [inputState, setInputState] = useState<string>("");
+  const [giftSwapState, setGiftSwapState] = useState<
+    { sender: string; receiver: string }[] | []
+  >([]);
 
   const handleSubmit = async () => {
-    if (!inputState) return
+    if (!inputState) return;
     const inputArray = inputState.trim().split(/[\n,]+/);
-    const giftSwapArr = await giftSwap(inputArray)
-    setGiftSwapState(giftSwapArr)
-  }
+    const giftSwapArr = await giftSwap(inputArray);
+    setGiftSwapState(giftSwapArr);
+  };
 
   const handleCopyToClipboard = async () => {
-    if (!inputState) return
-    const copyText = await pairingsToString(giftSwapState)
-    navigator.clipboard.writeText(copyText)
-  }
+    if (!inputState) return;
+    const copyText = await pairingsToString(giftSwapState);
+    navigator.clipboard.writeText(copyText);
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
@@ -30,12 +32,16 @@ export default function Home() {
           gift swap randomizer
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <div
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-          >
-            by{' '}
-            <a href='https://github.com/jallan07' rel='noreferrer' target='_blank'>
-              <span className="font-mono justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">josh<code className="font-mono font-bold"> allan</code></span>
+          <div className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
+            by{" "}
+            <a
+              href="https://www.joshallan.dev/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <span className="font-mono justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                josh<code className="font-mono font-bold"> allan</code>
+              </span>
             </a>
           </div>
         </div>
@@ -45,12 +51,14 @@ export default function Home() {
         <Box
           component="form"
           sx={{
-            '& .MuiTextField-root': { m: 1, width: '60ch', },
+            "& .MuiTextField-root": { m: 1, width: "60ch" },
           }}
           noValidate
           autoComplete="off"
         >
-          <h3 className='mx-2 my-4 font-mono font-bold'>add names separated by commas and/or new lines:</h3>
+          <h3 className="mx-2 my-4 font-mono font-bold">
+            add names separated by commas and/or new lines:
+          </h3>
           <TextField
             id="outlined-multiline-flexible"
             label="Participants"
@@ -59,17 +67,24 @@ export default function Home() {
             onBlur={(e) => setInputState(e.target.value)}
           />
         </Box>
-        <TextButton disabledState={false} handleClick={() => handleSubmit()} label="Submit" />
+        <TextButton
+          disabledState={false}
+          handleClick={() => handleSubmit()}
+          label="Submit"
+        />
       </div>
 
       {giftSwapState && giftSwapState.length > 0 && (
         <>
           <Divider className="my-5" variant="fullWidth" />
           <Pairings giftSwapState={giftSwapState} />
-          <TextButton disabledState={false} label={'copy to clipboard'} handleClick={() => handleCopyToClipboard()} />
+          <TextButton
+            disabledState={false}
+            label={"copy to clipboard"}
+            handleClick={() => handleCopyToClipboard()}
+          />
         </>
       )}
-
     </main>
-  )
+  );
 }
